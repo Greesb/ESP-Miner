fcbfa597ed7a9895b79d12c6f8a8b06f340d6063
Greesb <19546490+Greesb@users.noreply.github.com>
2026-09-08 18:37:33 +0200

    feat(axe-os): prettier address display for addresses with colon part

--- main/http_server/axe-os/src/app/pipes/address.pipe.ts
+++ main/http_server/axe-os/src/app/pipes/address.pipe.ts
@@ -1,8 +1,8 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-    name: 'address',
-    pure: true
+  name: 'address',
+  pure: true,
 })
 export class AddressPipe implements PipeTransform {
   private static _this = new AddressPipe();
@@ -17,7 +17,13 @@ export class AddressPipe implements PipeTransform {
     const maxLength = args?.length ?? 22;
 
     let segments: string[] = [];
-    for (let i = 0; i < value.length; i += 4) {
+    let segmentStartIdx = 0;
+    let colonIndex = value.indexOf(':');
+    if (colonIndex != -1) {
+      segmentStartIdx = colonIndex + 1;
+      segments.push(value.slice(0, colonIndex + 1));
+    }
+    for (let i = segmentStartIdx; i < value.length; i += 4) {
       segments.push(value.slice(i, i + 4));
     }
 
@@ -28,9 +34,13 @@ export class AddressPipe implements PipeTransform {
     let left = segments.slice(0, mid);
     let right = segments.slice(mid);
     do {
-      if (left.length > right.length) left.pop(); else right.shift();
+      if (left.length > right.length) left.pop();
+      else right.shift();
       formatted = left.join(' ') + '...' + right.join(' ');
-    } while (formatted.length > maxLength && (left.length > 1 || right.length > 1));
+    } while (
+      formatted.length > maxLength &&
+      (left.length > 1 || right.length > 1)
+    );
 
     return formatted;
   }
